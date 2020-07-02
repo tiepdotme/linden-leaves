12738fbe07b52cb4114244873f00f501dd8fe396
Matheus <matheus@e-com.club>
2020-07-01 21:24:09 -0300

    feat: new layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+if (window.innerWidth > 992) {
+  const menuDesk = document.getElementById('menu-novo')
+  const searchBarDesk = document.getElementById('search-bar')
+  const buttonsDesk = document.getElementById('headButtons')
+  const logoDesk = document.getElementById('coluna-logo')
+  logoDesk.classList.remove('col-lg-auto')
+  menuDesk.append(searchBarDesk)
+  menuDesk.append(buttonsDesk)
+}
